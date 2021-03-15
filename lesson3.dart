8fbd7b32a7c2d3e05361e8dd23c1840d25e1a53f
akburaevam <akburaevam@gmail.com>
2021-03-16 00:27:56 +0600

    16.03.2021 lesson 3

---
+++ lesson3.dart
@@ -0,0 +1,20 @@
+void main() {
+  
+  int a = 5;
+  int b = 10;
+  if (a < b) {
+    print ("true");
+  } else {
+    print ("false");
+  }     
+  // odnostrochnyi
+}
+void num (int a) {
+  for (int a = 0; a>5; a++);
+  print(a);
+}
+/* mnogo
+mnogo
+mnogostrochnyi */
+
+ /// documntation. blablabla vse, I'm done. Vremya 0:21 ///
